1481774fdcc8163f6d1b66ef74fe5ba7c937e74e
camposecokevin <rubelibolo@gmail.com>
2021-05-12 16:43:46 -0600

    conversor
    New data in py  code short

--- py_conversor/conversor.py
+++ py_conversor/conversor.py
@@ -1,7 +1,31 @@
-pesos = input("¿Cuántos pesos colombianos tienes?")
-pesos= float(pesos)
-valor_dolar = 3875
-dolares= pesos/valor_dolar
-dolares=round(dolares,2)
-dolares =str(dolares)
-print("tienes $"+ dolares + "dolares")
+menu = """
+
+BIENVENIDOS A MI CONVERTIDOR DE MONEDAS
+1- PESOS COLOMBIANOS
+2- PESOS ARGENTINOS
+3- PESOS MEXICANOS
+
+ELIGE UNA OPCIÓN:
+
+"""
+def conversor (tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos"  +tipo_pesos+  " tienes?")
+    pesos= float(pesos)
+    dolares= pesos/valor_dolar
+    dolares=round(dolares,2)
+    dolares =str(dolares)
+    print("tienes $"+ dolares + "dolares")    
+
+
+opcion = int(input(menu))
+if opcion ==1:
+    conversor("Colombianos", 3875)
+
+elif opcion ==2:
+    conversor("Argentinos", 65)
+elif opcion ==3:
+    conversor("mexicanos", 24)
+else:
+    print('Seleccione una opcion que se encuentre en el menu')
+
+
---
+++ py_conversor/funciones.py
@@ -0,0 +1,33 @@
+
+# def imprimir_mensaje():
+#     print("mensaje especial")
+#     print("Estoy aprendiendo a usar py")
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+# def conversion(mensaje):
+#     print('hola')
+#     print('como estás')
+#     print(mensaje)
+#     print('adios')
+
+
+# opcion = int(input('Elige una opción(1,2,3):'))
+# if opcion == 1:
+#     conversion('Eligiste la opcion 1')
+# elif opcion == 2:
+#     conversion('Eligiste la opcion 2')
+# elif opcion == 3:
+#     conversion('Eligiste la opcion 3')
+# else:
+#     print('elige alguna de las opciones')
+
+def suma(a,b)
+    print('se suma dos números')
+    resultado   =  a + b
+    return resultado
+
+sumatorio = suma(1,4)
+print(sumatorio)
