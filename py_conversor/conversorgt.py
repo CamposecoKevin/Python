843f0953cd261c144b096d30816e958db97b3010
camposecokevin <rubelibolo@gmail.com>
2021-05-12 12:34:40 -0600

    py1
    Introducce basic py

---
+++ py_conversor/conversorgt.py
@@ -0,0 +1,7 @@
+quetzales = input('¿Cuantos Quetzales tienes?' )
+quetzales = float(quetzales)
+valor_dolar = 7.7
+dolares = quetzales/valor_dolar
+dolares= round(dolares, 2)
+dolares = str(dolares)
+print('Tu tienes $'+ dolares + 'dolares')
