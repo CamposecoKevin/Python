87ca1428a5e5d2af59331ab9eb3ccc1d735b58ef
camposecokevin <rubelibolo@gmail.com>
2021-05-12 13:15:14 -0600

    Create condicionales.py
    Conditional

---
+++ py_conversor/condicionales.py
@@ -0,0 +1,14 @@
+# edad = int(input('Escribe su edad: '))
+# if edad > 17:
+#     print('eres mayor de edad')
+# else:
+#     print('eres menor de edada')
+
+numero = int(input("escriba un número : "))
+if numero < 0:
+    print('es un número negativo')
+elif numero == 0:
+    print('número que asigno es  0')
+else:
+    print('Es un numero mayor 0 ')
+
